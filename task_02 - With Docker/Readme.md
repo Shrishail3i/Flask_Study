384a20c6565041a07a91d7ec9b490b9639960ebd
Shrishail3i <shrishail.gajbhar@3i-infotech.com>
2022-11-15 11:43:40 +0530

    Added task-2 with Dockerfile

---
+++ task_02 - With Docker/Readme.md
@@ -0,0 +1,31 @@
+I am on Windows 10 running this under Ubuntu 18.08 environment using WSL.
+Here, While building from the dockerfile, it asked me:
+
+```
+Cannot connect to the Docker daemon at unix:///var/run/docker.sock. Is the docker daemon running?
+```
+
+One way here is to start the docker service..usually in linux we can start this using `systemctl start docker` but since we are in WSL, this gave me following message:
+
+```
+System has not been booted with systemd as init system (PID 1). Can't operate.
+```
+
+One way is to use other command `sudo service docker start`. (It worked!)
+
+Well..life is not easy it gave me another error
+
+```
+Got permission denied while trying to connect to the Docker daemon socket at unix:///var/run/docker.sock: Post "http://%2Fvar%2Frun%2Fdocker.sock/v1.24/build?buildargs=%7B%7D&cachefrom=%5B%5D&cgroupparent=&cpuperiod=0&cpuquota=0&cpusetcpus=&cpusetmems=&cpushares=0&dockerfile=Dockerfile&labels=%7B%7D&memory=0&memswap=0&networkmode=default&rm=1&shmsize=0&t=rest-apis-flask-python&target=&ulimits=null&version=1": dial unix /var/run/docker.sock: connect: permission denied
+```
+
+I think we need to add user to the docker group or some other hacks 
+Ref: https://stackoverflow.com/questions/51342810/how-to-fix-dial-unix-var-run-docker-sock-connect-permission-denied-when-gro
+
+I choose the following:
+
+```
+sudo chmod 666 /var/run/docker.sock
+```
+
+
---
+++ task_02 - With Docker/app.py
@@ -0,0 +1,44 @@
+# Import the Flask class from flask module
+from flask import Flask, request
+
+# Create a flask object with __name__ as parameter.
+app = Flask(__name__)
+
+# In this app, we want to manipulate the information regarding stores, so let us create a 
+# python variable for storing this information. It is a list of dictionary having name (string) and
+# items (dictionary) as keys.
+stores = [
+    {
+        "name": "MyStore",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
+# Implementing the endpoint for getting the stores information using API
+@app.get("/store")
+def get_stores():
+    return {"stores":stores}
+
+# Implementing the simple endpoint for adding the stores information using API
+@app.post("/store")
+def add_stores():
+    request_data = request.get_json()
+    new_store = {"name":request_data["name"], "items":[]}
+    stores.append(new_store)
+    return new_store, 201
+
+# Implementing the endpoint for adding the item to an existing store using API
+@app.post("/store/<string:name>/item")
+def add_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"]==name:
+            new_item = {"name":request_data["name"], "price": request_data["price"]}
+            store['items'].append(new_item)
+            return new_item, 201
+    return {"message": "store not found"}, 404
+
